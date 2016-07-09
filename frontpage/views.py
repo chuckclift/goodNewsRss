bab4ebe23b8963a8b99feeca6fdcd8a8afc0e651
Chuck Clift <chuckcclift@gmail.com>
2016-07-09 12:44:36 -0400

    added lemmatization to like/dislike views

--- frontpage/views.py
+++ frontpage/views.py
@@ -5,6 +5,8 @@ from django.core.urlresolvers import reverse
 
 from .models import Article, Article_Rating
 
+from nltk.stem import WordNetLemmatizer
+
 def index(request):
     article_list = Article.objects.order_by("-pub_date")[:20]
     fixed_titles = ["_".join(a.title.split()) for a in article_list]
@@ -15,8 +17,12 @@ def like(request, title):
     if not request.user.is_authenticated():
         return HttpResponseForbidden()
 
-    username = request.user.username
-    rating = Article_Rating.objects.create(user=username, title=title, rating=1)
+    wnl = WordNetLemmatizer()
+    lemmatized_headline = " ".join([wnl.lemmatize(a) for a in title.split()]) 
+
+    rating = Article_Rating.objects.create(user=request.user.username, 
+                                          title=lemmatized_headline, 
+                                          rating=1)
     rating.save()
     return HttpResponseRedirect('/')
 
@@ -24,8 +30,12 @@ def dislike(request, title):
     if not request.user.is_authenticated():
         return HttpResponseForbidden()
 
-    username = request.user.username
-    rating = Article_Rating.objects.create(user=username, title=title, rating=0)
+    wnl = WordNetLemmatizer()
+    lemmatized_headline = " ".join([wnl.lemmatize(a) for a in title.split()]) 
+
+    rating = Article_Rating.objects.create(user=request.user.username, 
+                                           title=lemmatized_headline,   
+                                           rating=0)
     rating.save()
     return HttpResponseRedirect('/')
 
